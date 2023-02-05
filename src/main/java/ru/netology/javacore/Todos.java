feb8bbee75b46062cb8ee3903ca53aaa308142d9
Denis <116478407+DenisBeluchenko@users.noreply.github.com>
2023-02-05 16:35:12 +0700

    Update Todos.java
    final

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -5,13 +5,14 @@ import java.util.stream.Collectors;
 
 public class Todos {
     private List<String> tasks;
+    private final int MAX_TASK = 7;
 
     public Todos() {
         this.tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        if (tasks.size() < 7) {
+        if (tasks.size() < MAX_TASK) {
             tasks.add(task);
         }
     }
